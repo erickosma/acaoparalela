16211941893a4efd943f0595c4a89aa94e4fc278
Erick Giorgio <erickosma@gmail.com>
2016-07-19 23:26:20 -0300

    Nova vercao laravel 5.2

--- config/app.php
+++ config/app.php
@@ -221,10 +221,10 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
 
-        'Form'      => Collective\Html\FormFacade::class,
+     /*   'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
         'Flash'     => Laracasts\Flash\Flash::class,
-
+*/
         'Form'  => Illuminate\Html\FormFacade::class,
         'HTML'  => Illuminate\Html\HtmlFacade::class,
         'Flash' => Laracasts\Flash\Flash::class,
