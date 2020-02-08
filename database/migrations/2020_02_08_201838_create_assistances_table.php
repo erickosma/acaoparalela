b0345d0a01ca9b6ca4afd298b2bda53afa776c68
Erick Giorgio <erick.silva@hotmart.com>
2020-02-08 17:39:25 -0300

    database model

---
+++ database/migrations/2020_02_08_201838_create_assistances_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssistancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assistances', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->string('title', 250)->index();
+            $table->text('description')->nullable();
+            $table->bigIncrements('sys_occupation_areas_id')->unsigned()->nullable();
+            $table->string('type', 20)->index();
+            $table->string('manual', 250)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assistances');
+    }
+}
