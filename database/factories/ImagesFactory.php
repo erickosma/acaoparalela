a52c1072891449cbf341493ea58cf7c90318c697
Erick Giorgio <erick.silva@hotmart.com>
2020-02-09 12:08:44 -0300

    image model

---
+++ database/factories/ImagesFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Images;
+use Faker\Generator as Faker;
+
+$factory->define(Images::class, function (Faker $faker) {
+    return [
+        'imageable_id' => function () {
+            $generatedUser =  rand(1,3);
+            switch ($generatedUser) {
+                case 1:
+                    return factory(App\Voluntary::class)->create()->id;
+                case 2:
+                    return factory(App\Ong::class)->create()->id;
+                case 3:
+                    return factory(App\Assistance::class)->create()->id;
+                default:
+                    return factory(App\User::class)->create()->id;
+            }
+        },
+        'imageable_type' => $faker->colorName //todo enum
+    ];
+});
