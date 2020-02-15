fda322374965bc959b9b2ccd7fa776c911a2269c
Erick Giorgio <erick.silva@hotmart.com>
2020-02-15 18:08:28 -0300

    database city and state

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(StateTableSeeder::class);
+        $this->call(CityTableSeed::class);
     }
 }
