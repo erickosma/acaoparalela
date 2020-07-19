bfef1227c7b456dbef9ba62b0a6b14b4a5e6c3f0
Erick Giorgio <erick.silva@hotmart.com>
2020-07-19 16:42:28 -0300

    git actions

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -12,7 +12,7 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testBasicTestExemple()
     {
         $userType = ImageType::ASSISTANCE;
         $this->assertTrue($userType == "ASSISTANCE" );
